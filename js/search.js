3cfb6ebc7aff340b8b86618a2b75d6ed1fd12c01
Paul McKibben <pmckibben@gmail.com>
2016-11-30 19:36:07 -0500

    Styling updates based on needs of subtheme

--- js/search.js
+++ js/search.js
@@ -7,13 +7,13 @@
     attach : function(context, settings) {
       if (context == document) {
         $('.form-search-expander').click(function(){
-          $(this).siblings('.form-search-main').show("slide", { direction: "right" }, 200);
+          $(this).siblings('.form-search-main').show(200);
         });
         $(window).scroll(function(){
           $('.form-search-expander').siblings('.form-search-main').hide();
         });
         $('.form-search-main-close').click(function(){
-          $(this).closest('.form-search-main').hide("slide", { direction: "right" }, 200);
+          $(this).closest('.form-search-main').hide(200);
         });
       }
     }
