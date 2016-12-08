293414d20af885cea4bc03085fe9cb9c5b8f1e89
Paul McKibben <pmckibben@gmail.com>
2016-12-08 11:56:59 -0500

    Javascript fix for menu toggle

--- js/menu_toggle.js
+++ js/menu_toggle.js
@@ -8,8 +8,7 @@
       if (context == document) {
         $('#menu-toggle').click(function(){
           var width = $('body').width();
-          var rightPosition = -(width - $('.region-top').width())/2;
-          $(this).siblings('ul.menu').css('width', width).css('right', rightPosition).slideToggle();
+          $(this).siblings('ul.menu').css('width', width).slideToggle();
         });
         $(window).resize(function(){
           $('#menu-toggle').siblings('ul.menu').hide();
